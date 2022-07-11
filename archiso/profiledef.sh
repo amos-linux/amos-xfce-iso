ac21e901ebe9f46ff0206101a47d36e41051e867
amanre <amanre556@gmail.com>
2022-07-11 02:37:24 +0200

    up

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2034
 
 iso_name="amos-xfce"
-iso_label="amos-xfce-v22.08.01"
+iso_label="amos-xfce-v22.08.02"
 iso_application="AmOs Live/Rescue CD"
-iso_version="v22.08.01"
+iso_version="v22.08.02"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
--- installation-scripts/rebuild.sh
+++ installation-scripts/rebuild.sh
@@ -15,7 +15,7 @@ echo
 	desktop="xfce"
 	dmDesktop="xfce"
 
-	amosVersion='v22.08.01'
+	amosVersion='v22.08.02'
 
 	isoLabel='amos-xfce-'$amosVersion'-x86_64.iso'
 
